069e14e0f8d518ce6fd267bd2ee3cf5b59d113d8
Mavis <mavismr98@gmail.com>
2025-04-22 16:18:26 -0300

    Adiciona novos módulos ao app.module.ts, incluindo Editora, LivroAutor, Autor, Categoria, LivroCategoria, LivroVenda e Venda.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { Module } from '@nestjs/common';
 
 import { PrismaModule } from './prisma/prisma.module';
 import { LivroModule } from './modules/livro/livro.module';
+import { EditoraModule } from './modules/editora/editora.module';
+import { LivroAutorModule } from './modules/livro-autor/livro-autor.module';
+import { AutorModule } from './modules/autor/autor.module';
+import { CategoriaModule } from './modules/categoria/categoria.module';
+import { LivroCategoriaModule } from './modules/livro-categoria/livro-categoria.module';
+import { LivroVendaModule } from './modules/livro-venda/livro-venda.module';
+import { VendaModule } from './modules/venda/venda.module';
 
 @Module({
-  imports: [PrismaModule, LivroModule],
+  imports: [
+    PrismaModule,
+    LivroModule,
+    EditoraModule,
+    LivroAutorModule,
+    AutorModule,
+    CategoriaModule,
+    LivroCategoriaModule,
+    LivroVendaModule,
+    VendaModule,
+  ],
   controllers: [],
   providers: [],
 })
