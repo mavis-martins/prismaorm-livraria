c9d9a4426bac58e82314a588c207768a32f053c4
Mavis <mavismr98@gmail.com>
2025-04-22 16:18:35 -0300

    Adiciona o PrismaService como provedor no módulo Editora.

--- src/modules/editora/editora.module.ts
+++ src/modules/editora/editora.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EditoraService } from './editora.service';
 import { EditoraController } from './editora.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [EditoraService],
+  providers: [EditoraService, PrismaService],
   controllers: [EditoraController],
 })
 export class EditoraModule {}
